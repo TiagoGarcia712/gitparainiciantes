422f1a1529d5607adc769c5047e4fa249d22607e
TiagoCosta <tiago.costa@projuris.com.br>
2022-04-19 13:08:06 -0300

    nossa primeira api

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Olá mundo estou funcionando!' });
+})
+app.get('/Olá', (req, res) => {
+    res.status(200).send({ message: 'Oi sou a rota Olá' });
+})
+
+app.listen(4001, ()=>{
+    console.log('Api rodando na porta 4001');
+})
